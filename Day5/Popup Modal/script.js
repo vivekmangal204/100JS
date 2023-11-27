c001a453bb6097858d3c8c66d517ee7b92cbe75b
VIVEK MANGAL <138361918+vivekmangal204@users.noreply.github.com>
2023-11-27 01:10:20 +0000

    Day 5 Done

---
+++ Day5/Popup Modal/script.js
@@ -0,0 +1,7 @@
+let popup = document.querySelector('.popup');
+let openModal = () => {
+    popup.classList.add('popup_open');
+}
+let closeModal = () => {
+    popup.classList.remove('popup_open');
+}
