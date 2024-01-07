4f8d79eec270d0b2b12a4136ca6736b08afba95e
VIVEK MANGAL <138361918+vivekmangal204@users.noreply.github.com>
2024-01-07 01:39:26 +0000

    day46done

---
+++ Day46/Weather App/script.js
@@ -0,0 +1,49 @@
+const apiKey = "424992186eca6597384674ffd97283b9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city)
+{
+    const response = await fetch(apiUrl+ city +`&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+        var data = await response.json();
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp)  + "°c";
+        document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML=data.wind.speed + " Km/hr";
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src="Images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear")
+        {
+            weatherIcon.src="Images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain")
+        {
+            weatherIcon.src="Images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle")
+        {
+            weatherIcon.src="Images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist")
+        {
+            weatherIcon.src="Images/mist.png";
+        }
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display="none";
+    }
+    
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
